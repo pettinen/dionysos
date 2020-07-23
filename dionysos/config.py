1c86ecdb80b9277dd28ef3c9a354cdc2d77934fc
Jeremias Pettinen <hi@jeremi.as>
2020-07-23 15:10:12 +0000

    Add configuration options

--- dionysos/config.py
+++ dionysos/config.py
@@ -1,6 +1,7 @@
 ADMIN_EMAIL = 'hi@jeremi.as'
 APPLICATION_NAME = 'Dionysos'
 APPLICATION_ROOT = '/'
+COOKIE_PATH = '/'
 CSRF_COOKIE = 'csrf_token'
 CSRF_HEADER = 'X-CSRF-Token'
 CSRF_DATA_KEY = 'csrf'
@@ -9,7 +10,7 @@ JWT_COOKIE = 'jwt'
 LANGUAGES = ['en']
 DEFAULT_LANGUAGE = 'en'
 LANGUAGE_COOKIE = 'lang'
-LOG_FILE = '/var/log/dionysos/dionysos.log'
+LOG_FILE = 'dionysos.log'
 PREFERRED_URL_SCHEME = 'https'
 PSYCOPG2_DB = 'dbname=dionysos'
 REDIS_URL = 'unix:///run/redis/redis.sock?db=0'
@@ -18,14 +19,15 @@ SERVER_NAME = 'dionysos.jeremi.as'
 SESSION_COOKIE_SECURE = True
 TESTING = True
 
-GAME_ID_LENGTH = 6
+GAME_ID_LENGTH = 5
 PASSWORD_MIN_LENGTH = 6
 USERNAME_MAX_LENGTH = 25
+GAME_NAME_MAX_LENGTH = 32
 
 CARDS_URL = 'img/cards/'
 CARD_IMG_EXT = 'webp'
 CARD_BASES_URL = 'img/card-bases/'
-MAX_CREATED_GAMES = 2
+MAX_GAMES_PER_USER = 2
 MAX_PLAYERS_MIN = 1
 MAX_PLAYERS_MAX = 10
 MAX_PLAYERS_DEFAULT = MAX_PLAYERS_MAX
